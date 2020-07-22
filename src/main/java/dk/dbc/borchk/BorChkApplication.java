a44456554fee761e57ace45f689d7fbd0cc6fb0f
thl <thl@dbc.dk>
2020-07-22 08:04:41 +0200

    IS-280: BorChk - initial project

--- src/main/java/dk/dbc/borchk/BorChkApplication.java
+++ src/main/java/dk/dbc/borchk/BorChkApplication.java
@@ -2,7 +2,6 @@ package dk.dbc.borchk;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
